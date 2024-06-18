563e7c3313f706d4eeeac4ec6081375a9c00f82c
Kholopkin JackD161 Yuriy <holopkin_yurik@mail.ru>
2024-06-19 01:29:52 +0300

    first iteration

--- src/main/java/ParserFile.java
+++ src/main/java/ParserFile.java
@@ -1,3 +1,51 @@
-public class ParserFile {
+import java.io.*;
+import java.util.ArrayList;
 
+public class ParserFile {
+    private final ArrayList<StockItem> items;
+    private final String fileName;
+    int cnt = 0;
+
+    public ParserFile(String fileName) {
+        this.fileName = fileName;
+        items = new ArrayList<>();
+        readFile();
+    }
+
+    private void readFile() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+            while (reader.ready()) {
+                String sgtin = getString(reader);
+                String mnn = getString(reader);
+                String tradeName = getString(reader);
+                String financeType = getString(reader);
+                items.add(new StockItem(sgtin, mnn, tradeName, financeType));
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("Файл не обнаружен");
+        }
+        catch (IOException e) {
+            System.err.println("Файл не готов для чтения");
+        }
+    }
+    private String getString(BufferedReader reader) throws IOException {
+        String line = reader.readLine();
+        if (line.isEmpty()) {
+            cnt++;
+            getString(reader);
+        }
+        else {
+            return line;
+        }
+        return line;
+    }
+
+    public ArrayList<StockItem> getItems() {
+        return items;
+    }
+    public int getCnt() {
+        return cnt;
+    }
 }
--- src/main/java/StockItem.java
+++ src/main/java/StockItem.java
@@ -1,13 +1,29 @@
 public class StockItem {
-    private String sgtin; // SGTIN
-    private String mnn; // МНН
-    private String tradeName; // Торговое найменование
-    private String financeType; // Тип финансирования
+    private final String sgtin; // SGTIN
+    private final String mnn; // МНН
+    private final String tradeName; // Торговое найменование
+    private final String financeType; // Тип финансирования
 
     public StockItem(String sgtin, String mnn, String tradeName, String financeType) {
         this.sgtin = sgtin;
         this.mnn = mnn;
         this.tradeName = tradeName;
         this.financeType = financeType;
+    }
+
+    public String getSgtin() {
+        return sgtin;
+    }
+
+    public String getMnn() {
+        return mnn;
+    }
+
+    public String getTradeName() {
+        return tradeName;
+    }
+
+    public String getFinanceType() {
+        return financeType;
     }
 }
