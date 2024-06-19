ca79a7331c3c7e2abda335e4d6037b8698c06571
Kholopkin JackD161 Yuriy <holopkin_yurik@mail.ru>
2024-06-19 08:35:27 +0300

    first iteration

---
+++ src/main/java/MainClassTest.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class MainClassTest {
+
+    public static void main(String[] args) {
+        // локальные файлы
+        String fileIn = "C:\\Users\\holop\\IdeaProjects\\ru.JackD161.ParserMDLPstocks\\tmp\\stock922.txt";
+        String fileOut = "C:\\Users\\holop\\IdeaProjects\\ru.JackD161.ParserMDLPstocks\\tmp\\stock922_out.csv";
+        ParserFile parser = new ParserFile(fileIn);
+        ArrayList<StockItem> stockItems = parser.getItems();
+        System.out.println(parser.getCnt());
+        try {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileOut)));
+            for (StockItem item : stockItems) {
+            String out = item.getSgtin() + "\t" + item.getMnn() + "\t" + item.getTradeName() + "\t" + item.getFinanceType() + "\n";
+            writer.write(out);
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("Файл не найден для записи");
+        }
+        catch (IOException e) {
+            System.err.println("Ошибка записи файла");
+        }
+    }
+
+}
