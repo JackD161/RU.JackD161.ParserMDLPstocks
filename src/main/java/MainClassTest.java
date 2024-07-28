55c6c95a4bb101f713080137ac8bd7f73b74c3c5
Kholopkin JackD161 Yuriy <holopkin_yurik@mail.ru>
2024-07-28 21:54:42 +0300

    Обработчик работает с 6 строками

--- src/main/java/MainClassTest.java
+++ src/main/java/MainClassTest.java
@@ -4,11 +4,10 @@ public class MainClassTest {
 
     public static void main(String[] args) {
         // локальные файлы
-        String fileIn = "P:\\УАС\\Холопкин\\Остатки МДЛП\\1_Сальск\\отгружен после 28 марта.txt";
-        String fileOutXls = "P:\\УАС\\Холопкин\\Остатки МДЛП\\1_Сальск\\Отгружен_до_28_03_2021";
+        String fileIn = "P:\\УАС\\Холопкин\\Остатки МДЛП\\ФД\\СОБРАНО\\16_Республика Крым, г.о. Феодосия, г Феодосия, б-р Старшинова, д. 8\\отгружено после.txt";
+        String fileOutXls = "P:\\УАС\\Холопкин\\Остатки МДЛП\\ФД\\СОБРАНО\\16_Республика Крым, г.о. Феодосия, г Феодосия, б-р Старшинова, д. 8\\Отгружено_после_28_03_2021";
         ParserFile parser = new ParserFile(fileIn);
         ArrayList<StockItem> stockItems = parser.getItems();
         ExcellWriter.SaveData2File(fileOutXls, stockItems);
     }
-
 }
--- src/main/java/ParserFile.java
+++ src/main/java/ParserFile.java
@@ -1,29 +1,28 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class ParserFile {
     private final ArrayList<StockItem> items;
+    private final ArrayList<String> lines;
     private final String fileName;
 
     public ParserFile(String fileName) {
         this.fileName = fileName;
         items = new ArrayList<>();
+        lines = new ArrayList<>();
         readFile();
+        clear(lines);
+        infillStockItems();
     }
 
     private void readFile() {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
-            while (reader.ready()) {
-                String sgtin = getString(reader);
-                String mnn = getString(reader);
-                String tradeName = getString(reader);
-                String seria = getString(reader);
-                String expireDate = getString(reader);
-                String financeType = getString(reader);
-                items.add(new StockItem(sgtin, mnn, tradeName, seria, expireDate, financeType));
+            while(reader.ready()) {
+            lines.add(reader.readLine());
             }
-        }
+            }
         catch (FileNotFoundException e) {
             System.err.println("Файл не обнаружен");
         }
@@ -31,17 +30,33 @@ public class ParserFile {
             System.err.println("Файл не готов для чтения");
         }
     }
-    private String getString(BufferedReader reader) throws IOException {
-        String line = reader.readLine();
-        if (line.isEmpty()) {
-            getString(reader);
+    private void clear(ArrayList<String> lines) {
+        System.out.println(lines.size());
+        ListIterator<String> iterator = lines.listIterator();
+        while (iterator.hasNext()) {
+            String s = iterator.next();
+            if (s.isEmpty()) {
+                iterator.remove();
+            }
         }
-        else {
-            return line;
-        }
-        return line;
+        System.out.println(lines.size());
     }
-
+    private String getString(ArrayList<String> lines) {
+        String string = lines.get(0);
+        lines.remove(0);
+        return string;
+    }
+    private void infillStockItems() {
+        while (!lines.isEmpty()) {
+            String sgtin = getString(lines);
+            String mnn = getString(lines);
+            String tradeName = getString(lines);
+            String financeType = getString(lines);
+            String seria = getString(lines);
+            String expireDate = getString(lines);
+            items.add(new StockItem(sgtin, mnn, tradeName, financeType, seria, expireDate));
+        }
+    }
     public ArrayList<StockItem> getItems() {
         return items;
     }
